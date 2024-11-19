1cd09942a529a40fe94b015ca28e822e34fdf61e
Wayback2010 <wayback2010@outlook.com>
2024-11-19 10:34:30 -0600

    Add files via upload
    The raw Python code for RTOWO

---
+++ RTOWO.py
@@ -0,0 +1,19 @@
+# wegwegtfuwwy and painfuwwy made by Wayback2010
+import keyboard, time, uwuify
+
+print('Running...')
+
+while True:
+    if keyboard.is_pressed('r'):
+        time.sleep(0.05)
+        keyboard.press('backspace')
+        keyboard.release('backspace')
+        keyboard.press('w')
+        keyboard.release('w')
+        typed=True
+    if keyboard.is_pressed('l'):
+        time.sleep(0.05)
+        keyboard.press('backspace')
+        keyboard.release('backspace')
+        keyboard.press('w')
+        keyboard.release('w')
