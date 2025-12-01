9c7c3d2fe2c49cb1ba99f6353e5a0c7d7145270e
Wayback2010 <wayback2010@outlook.com>
2025-12-01 14:38:12 -0600

    Added emoticon functionality

--- RTOWO.py
+++ RTOWO.py
@@ -1,19 +1,28 @@
-# wegwegtfuwwy and painfuwwy made by Wayback2010
-import keyboard, time, uwuify
+import keyboard, random
 
-print('Running...')
+period_emoticon = ['uwu',  '>w<', '^w^',  ':3', '>:3', 'nwn']
+exclamation_emoticon = ['UwU', 'UWU', 'x3', ':3']
+question_emoticon = ['owo', 'OWO', 'OwO', ':3c', '>:3c']
 
-while True:
-    if keyboard.is_pressed('r'):
-        time.sleep(0.05)
-        keyboard.press('backspace')
-        keyboard.release('backspace')
-        keyboard.press('w')
-        keyboard.release('w')
-        typed=True
-    if keyboard.is_pressed('l'):
-        time.sleep(0.05)
-        keyboard.press('backspace')
-        keyboard.release('backspace')
-        keyboard.press('w')
-        keyboard.release('w')
+def intercept_keys(event):
+    if event.event_type == 'down' and event.name in ['l', 'r']:
+        keyboard.write('w')
+        return False
+    elif event.event_type == 'down' and event.name in ['L', 'R']:
+        keyboard.write('W')
+        return False
+    elif event.event_type == 'down' and event.name in ['.', ',']:
+        keyboard.write(f' {random.choice(period_emoticon)}{event.name}')
+        return False
+    elif event.event_type == 'down' and event.name == '!':
+        keyboard.write(f' {random.choice(exclamation_emoticon)}{event.name}')
+        return False
+    elif event.event_type == 'down' and event.name == '?':
+        keyboard.write(f' {random.choice(question_emoticon)}{event.name}')
+        return False
+    else:
+        return True
+
+keyboard.hook(intercept_keys, suppress=True)
+print('Running, close the window to exit.')
+keyboard.wait()
